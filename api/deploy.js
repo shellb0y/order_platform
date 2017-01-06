c2ab0b9836ba374c7846fa413d7e843a0d702bcb
zt.melody <zt.freedom@gmail.com>
2017-01-06 11:58:42 +0800

    更新部署API

--- api/deploy.js
+++ api/deploy.js
@@ -8,17 +8,22 @@ const path = require('path');
 
 
 router.post('/github', async(ctx, next) => {
-    logger.i('deploy', ctx.request.body, 'order_platform');
-    exec(path.join(process.cwd(), '/server_production/deploy.sh'), function (error, stdout, stderr) {
-        logger.i('deploy', stdout, 'order_platform');
-        logger.e('deploy', `stderr:${stderr}`, 'order_platform');
-        if (error !== null) {
-            logger.e('deploy', `exec error:${error}`, 'order_platform');
-        }
-    });
+    if (ctx.request.body.ref == 'refs/heads/master') {
+        logger.i('deploy', ctx.request.body, 'order_platform');
+        exec(path.join(process.cwd(), '/server_production/deploy.sh'), function (error, stdout, stderr) {
+            logger.i('deploy', stdout, 'order_platform');
+            logger.e('deploy', `stderr:${stderr}`, 'order_platform');
+            if (error !== null) {
+                logger.e('deploy', `exec error:${error}`, 'order_platform');
+            }
+        });
 
-    logger.i('deploy', 'Deploy Done', 'order_platform');
-    ctx.body = 'Deploy Done.';
+        logger.i('deploy', 'Deploy Done', 'order_platform');
+        ctx.body = 'Deploy Done.';
+    } else {
+        ctx.status = 201;
+        ctx.body = 'Not master.Deploy abort.';
+    }
 });
 
 module.exports = router;
